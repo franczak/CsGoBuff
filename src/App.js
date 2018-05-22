860573302ef5d0033db3134f959a138cf41a7034
Jan Ignasik <ignasikj@gmail.com>
2018-05-22 22:52:22 +0200

    simple auth routing

--- src/App.js
+++ src/App.js
@@ -3,22 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 import Container from './Components/CardContainer';
 import LoginButton from "./Components/LoginButton/index";
-
+import axios from 'axios'
 
 class App extends Component {
 
+  state = {
+    showApp: false
+  };
+
+  componentDidMount = () => {
+    axios.get('https://cs-go-buff.herokuapp.com/user', {withCredentials: true}).then(res => {
+      console.log(res)
+      if(!!res.data) {
+          this.setState({showApp: true})
+      }
+    })
+  };
+
   render() {
-    return (
-      <div className="App">
+    return <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-          <br/>
-          <Container/>
-          <LoginButton />
+        <br/>
+        {
+          this.state.showApp?
+              <Container/>
+              :
+              <LoginButton />
+        }
       </div>
-    );
+
   }
 }
 
--- src/Components/LoginButton/index.js
+++ src/Components/LoginButton/index.js
@@ -4,27 +4,12 @@ import axios from 'axios'
 
 export default class LoginButton extends Component {
 
-    componentDidMount() {
-
-    }
-
-
-    showUser = () => {
-        axios.get("http://localhost:5000").then((res) => {
-            console.log(res)
-        })
-    };
-
     render() {
         return(
             <div>
-                <a href="http://localhost:5000/authenticate" >
+                <a href="https://cs-go-buff.herokuapp.com/" >
                     <img src={'https://steamcommunity-a.akamaihd.net/public/images/signinthroughsteam/sits_02.png'} />
                 </a>
-
-                <div onClick={this.showUser}>
-                    KUUUURWa
-                </div>
             </div>
 
         )
