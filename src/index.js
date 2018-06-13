2a14e420a1ca1250628c726a4733efb7638c7643
Jan Ignasik <ignasikj@gmail.com>
2018-06-13 21:16:41 +0200

    use dotenv in development

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config()
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
