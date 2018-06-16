53ddbe6e1311c7cb51436b2b6ee5a4cc45f38054
Jan Ignasik <ignasikj@gmail.com>
2018-06-16 13:16:16 +0200

    Refactor and simpler api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import axios from 'axios';
 import './App.css';
 import Container from './Components/CardContainer';
 import LoginButton from './Components/LoginButton/index';
+import { ToastContainer, toast } from 'react-toastify';
 
 class App extends Component {
     state = {
@@ -19,18 +20,23 @@ class App extends Component {
 
     render() {
       return (
-        <div className="App">
-          <header className="App-header">
-            <h1 className="App-title">STEAM STATS FRIENDS</h1>
-          </header>
-          <br />
-          {
-                    this.state.showApp ?
-                      <Container />
-                        :
-                      <LoginButton />
-                }
-        </div>);
+        <Fragment>
+          <div className="App">
+            <ToastContainer toastClassName={'toast'} />
+            <header className="App-header">
+              <h1 className="App-title">STEAM STATS FRIENDS</h1>
+            </header>
+            <br />
+            {
+              this.state.showApp ?
+                <Container />
+                :
+                <LoginButton />
+            }
+          </div>
+        </Fragment>
+
+      );
     }
 }
 
